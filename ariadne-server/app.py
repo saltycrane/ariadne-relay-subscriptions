0c80301adbf675ecd83aa3d5f14f1d5fab7f8c83
Eliot <saltycrane@gmail.com>
2021-12-09 10:29:13 -0800

    add mutations and queries

--- ariadne-server/app.py
+++ ariadne-server/app.py
@@ -1,13 +1,10 @@
-from ariadne import QueryType, make_executable_schema, load_schema_from_path
+from ariadne import make_executable_schema, load_schema_from_path, snake_case_fallback_resolvers
 from ariadne.asgi import GraphQL
+
+from mutations import mutation
+from queries import query
 
 type_defs = load_schema_from_path("../schema.graphql")
 
-query = QueryType()
-
-@query.field("hello")
-def resolve_hello(*_):
-    return "Hello world"
-
-schema = make_executable_schema(type_defs, query)
+schema = make_executable_schema(type_defs, query, mutation, snake_case_fallback_resolvers)
 app = GraphQL(schema, debug=True)
