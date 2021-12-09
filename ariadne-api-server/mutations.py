f5663fa0e2a9f3cf10b9fc836babf707e856ddc7
Eliot <saltycrane@gmail.com>
2021-12-09 15:50:14 -0800

    backend change to allow returning existing user

--- ariadne-api-server/mutations.py
+++ ariadne-api-server/mutations.py
@@ -8,7 +8,13 @@ mutation = ObjectType("Mutation")
 @convert_kwargs_to_snake_case
 async def resolve_create_user(obj, info, username):
     try:
-        if not users.get(username):
+        existing_user = users.get(username)
+        if existing_user:
+            return {
+                "success": True,
+                "user": existing_user,
+            }
+        else:
             user = {
                 "user_id": len(users) + 1,
                 "username": username,
@@ -18,10 +24,6 @@ async def resolve_create_user(obj, info, username):
                 "success": True,
                 "user": user,
             }
-        return {
-            "success": False,
-            "errors": ["Username is taken"],
-        }
     except Exception as error:
         return {
             "success": False,
