d4356e28c71d9c3465f13ac5eb053bd5723ecf6b
Eliot <saltycrane@gmail.com>
2021-12-23 15:40:48 -0800

    useState to store userId

--- react-relay-webapp/src/EnterName.tsx
+++ react-relay-webapp/src/EnterName.tsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import { graphql, useMutation } from "react-relay";
 import { Button, Input } from "reactstrap";
 
-export default function EnterName() {
+import { EnterNameMutation } from "./__generated__/EnterNameMutation.graphql";
+
+type TProps = {
+  onGetUserId: (userId: string) => void;
+};
+
+export default function EnterName({ onGetUserId }: TProps) {
   const [name, setName] = useState("");
 
-  const [createUser] = useMutation(
+  const [createUser] = useMutation<EnterNameMutation>(
     graphql`
       mutation EnterNameMutation($username: String!) {
         createUser(username: $username) {
@@ -20,26 +26,28 @@ export default function EnterName() {
   return (
     <div>
       <h1 className="fs-3 mb-3">Enter your name</h1>
-      <Input
-        className="mb-3"
-        onChange={(event) => setName(event.target.value)}
-        type="text"
-        value={name}
-      />
-      <Button
-        block
-        color="primary"
-        onClick={() => {
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
           createUser({
             onCompleted: (data) => {
-              console.log(data);
+              const userId = data.createUser.user.userId;
+              onGetUserId(userId);
             },
             variables: { username: name },
           });
         }}
       >
-        Go
-      </Button>
+        <Input
+          className="mb-3"
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          value={name}
+        />
+        <Button block color="primary">
+          Go
+        </Button>
+      </form>
     </div>
   );
 }
--- react-relay-webapp/src/index.tsx
+++ react-relay-webapp/src/index.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { RelayEnvironmentProvider } from "react-relay";
 
 import EnterName from "./EnterName";
 import { environment } from "./relay";
 
 export default function Index() {
+  const [userId, setUserId] = useState("");
+
   return (
     <RelayEnvironmentProvider environment={environment}>
       <div className="align-items-center bg-light d-flex h-100 justify-content-center">
-        <EnterName />
+        {userId ? <div>{userId}</div> : <EnterName onGetUserId={setUserId} />}
       </div>
     </RelayEnvironmentProvider>
   );
